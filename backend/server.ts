efb00aa3ebcd28b2c072ca5e1685929517f2a590
Robbie M <robbie@roofbuddy.co.nz>
2023-06-30 23:32:01 +0800

    feat: add scripts

---
+++ backend/server.ts
@@ -0,0 +1,24 @@
+import express, { Express, Request, Response } from 'express';
+import products from './data/products.js';
+
+const port = 8000;
+
+const app: Express = express();
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('API is running...');
+});
+
+app.get('/api/products', (_req: Request, res: Response) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (_req: Request, res: Response) => {
+  const product = products.find((p) => p._id === _req.params.id);
+
+  res.json(product);
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running on port ${port}`);
+});
