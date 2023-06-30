d71478a78c5b2dbc0a0819e6ff2994eb3db4ac39
Robbie M <robbie@roofbuddy.co.nz>
2023-06-30 22:35:04 +0800

    feat: add product screen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,11 +10,13 @@ import {
 import './assets/bootstrap.custom.css';
 import './assets/index.css';
 import HomeScreen from './screens/HomeScreen.tsx';
+import ProductScreen from './screens/ProductScreen.tsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
 );
