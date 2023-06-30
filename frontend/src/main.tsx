1ab00e09dee41295722d0e7dfbad52610b8a2e48
Robbie M <robbie@roofbuddy.co.nz>
2023-06-30 22:21:07 +0800

    feat: add react-router-dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import './assets/bootstrap.custom.css';
 import './assets/index.css';
+import HomeScreen from './screens/HomeScreen.tsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
